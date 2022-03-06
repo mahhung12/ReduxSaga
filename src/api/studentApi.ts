875bcc918ff5464ea2d9f88e342791d41bf5adf4
mhung286 <mahhungg.12@gmail.com>
2022-03-06 20:36:16 +0700

    Lesson 13

---
+++ src/api/studentApi.ts
@@ -0,0 +1,31 @@
+import { ListParams, Student, ListResponse } from "models";
+import axiosClient from "./axiosClient";
+
+const studentApi = {
+  getAll(params: ListParams): Promise<ListResponse<Student>> {
+    const url = "/students";
+    return axiosClient.get(url, { params });
+  },
+
+  getById(id: string): Promise<Student> {
+    const url = `/students/${id}`;
+    return axiosClient.get(url);
+  },
+
+  add(data: Student): Promise<Student> {
+    const url = "/students";
+    return axiosClient.post(url, { data });
+  },
+
+  update(data: ListParams): Promise<Student> {
+    const url = "/students";
+    return axiosClient.patch(url, { data });
+  },
+
+  remove(id: string): Promise<any> {
+    const url = `/students/${id}`;
+    return axiosClient.delete(url);
+  },
+};
+
+export default studentApi;
