ced8f928af57638fd8cc65b3c4615c526f65eed1
mhung286 <mahhungg.12@gmail.com>
2022-03-07 16:55:11 +0700

    Material UI to ReactJS TypeScript

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,11 +1,7 @@
 import counterSaga from "features/counter/counterSaga";
 import { all } from "redux-saga/effects";
 
-function* helloSaga() {
-  console.log("Hello Saga");
-}
-
 export default function* rootSaga() {
   console.log("root saga");
-  yield all([helloSaga(), counterSaga()]);
+  yield all([counterSaga()]);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <CssBaseline />
         <App />
       </BrowserRouter>
     </Provider>
