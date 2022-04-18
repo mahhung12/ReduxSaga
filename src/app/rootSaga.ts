89c8ebfe32a8746a5b611bbe151465630540fffe
mhung286 <mahhungg.12@gmail.com>
2022-04-18 14:14:45 +0700

    Config Slice and Saga for Student Feature

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,9 +1,10 @@
 import { authSaga } from "features/auth/authSaga";
 import counterSaga from "features/counter/counterSaga";
 import dashboardSaga from "features/dashboard/dashboardSaga";
+import studentSaga from "features/student/studentSaga";
 import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
   console.log("root saga");
-  yield all([counterSaga(), authSaga(), dashboardSaga()]);
+  yield all([counterSaga(), authSaga(), dashboardSaga(), studentSaga()]);
 }
--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,7 +1,20 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch } from 'app/hooks';
+import { studentActions } from '../studentSlice';
 
 
 export default function ListPage() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(
+      studentActions.fetchStudentList({
+        _page: 1,
+        _limit: 15,
+      })
+    );
+  }, [dispatch])
+
   return (
     <div>
       Student List Page
