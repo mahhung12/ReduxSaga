067349f022489193073a8e3b704013ca8be741d3
mhung286 <mahhungg.12@gmail.com>
2022-04-20 18:11:48 +0700

    Sort and Clear Students

--- src/features/student/components/StudentFilters.tsx
+++ src/features/student/components/StudentFilters.tsx
@@ -1,7 +1,7 @@
-import { Box, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select } from '@material-ui/core';
+import { Box, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select, Button } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import { City, ListParams } from 'models';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useRef } from 'react';
 
 export interface StudentFiltersProps {
     filter: ListParams;
@@ -14,6 +14,8 @@ export interface StudentFiltersProps {
 export default function StudentFilters({
     filter, cityList, onChange, onSearchChange
 }: StudentFiltersProps) {
+    const searchRef = useRef<HTMLInputElement>();
+
     const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (!onSearchChange) return;
 
@@ -36,6 +38,37 @@ export default function StudentFilters({
         onChange(newFilter);
     }
 
+    const handleSortChange = (e: ChangeEvent<{ name?: string; value: unknown }>) => {
+        if (!onChange) return;
+
+        const value = e.target.value;
+        const [_sort, _order] = (value as string).split('.')
+        const newFilter: ListParams = {
+            ...filter,
+            _sort: _sort || undefined,
+            _order: (_order as 'asc' | 'desc') || undefined,
+        }
+        onChange(newFilter);
+    }
+
+    const handleClearFilter = () => {
+        if (!onChange) return;
+
+        const newFilter: ListParams = {
+            ...filter,
+            _page: 1,
+            _sort: undefined,
+            _order: undefined,
+            city: undefined,
+            name_like: undefined,
+        }
+        onChange(newFilter);
+
+        if (searchRef.current) {
+            searchRef.current.value = '';
+        }
+    }
+
     return (
         <Box>
             <Grid container spacing={3}>
@@ -48,6 +81,7 @@ export default function StudentFilters({
                             endAdornment={<Search />}
                             defaultValue={filter.name_like}
                             onChange={handleSearchChange}
+                            inputRef={searchRef}
                         />
                     </FormControl>
                 </Grid>
@@ -73,6 +107,39 @@ export default function StudentFilters({
                         </Select>
                     </FormControl>
                 </Grid>
+
+                <Grid item xs={12} md={6} lg={2}>
+                    <FormControl fullWidth size="small" variant="outlined">
+                        <InputLabel id="sortBy">Sort By</InputLabel>
+                        <Select
+                            labelId="sortBy"
+                            value={filter._sort ? `${filter._sort}.${filter._order}` : ''}
+                            onChange={handleSortChange}
+                            label="Sort By"
+                        >
+                            <MenuItem value="">
+                                <em>No Sort</em>
+                            </MenuItem>
+
+                            <MenuItem value="name.asc"> Name ASC </MenuItem>
+                            <MenuItem value="name.desc"> Name DESC </MenuItem>
+                            <MenuItem value="mark.asc"> Mark ASC </MenuItem>
+                            <MenuItem value="mark.desc"> Mark DESC </MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item xs={12} md={6} lg={1}>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        onClick={handleClearFilter}
+                        fullWidth
+                    >
+                        Clear
+                    </Button>
+                </Grid>
+
             </Grid>
         </Box >
     );
