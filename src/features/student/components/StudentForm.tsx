7c1bd66248f3f18e782f4084277aa3a83d7539f6
mhung286 <mahhungg.12@gmail.com>
2022-04-21 13:48:03 +0700

    Setup RadioGroupField Control

--- src/features/student/components/StudentForm.tsx
+++ src/features/student/components/StudentForm.tsx
@@ -1,5 +1,6 @@
 import { Box, Button } from '@material-ui/core';
 import { InputField } from 'components/FormFields';
+import { RadioGroupField } from 'components/FormFields/RadioGroupField';
 import { Student } from 'models';
 import { useForm } from 'react-hook-form';
 
@@ -22,9 +23,16 @@ export default function StudentForm({ initialValue, onSubmit }: StudentFormProps
             <form onSubmit={handleSubmit(handleFormSubmit)}>
                 {/* FORM FIELD */}
                 <InputField name="name" control={control} label="Full Name" />
+
+                <RadioGroupField name="gender" control={control} label="Gender"
+                    options={[
+                        { label: "Male", value: "male" },
+                        { label: "Female", value: "female" },
+                    ]}
+                />
+
                 <InputField name="age" control={control} label="Age" type="number" />
                 <InputField name="mark" control={control} label="Mark" type="number" />
-                <InputField name="gender" control={control} label="Gender" />
                 <InputField name="city" control={control} label="City" />
 
                 <Box mt={2}>
@@ -33,4 +41,4 @@ export default function StudentForm({ initialValue, onSubmit }: StudentFormProps
             </form>
         </Box>
     );
-}
+}       
